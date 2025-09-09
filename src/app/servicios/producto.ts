660b5e9323b0cdcb8dd5bbbd260e57e662f82aae
JuanPichus <juanpichus123@gmail.com>
2025-09-08 20:04:14 -0600

    I have been tryed to implement the HTTP GET request from a XML file, but i havent finished yet

--- src/app/servicios/producto.ts
+++ src/app/servicios/producto.ts
@@ -1,16 +1,28 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, Observable } from 'rxjs';
 import { Producto } from '../modelos/producto';
 
-@Injectable({
-  providedIn: 'root',
-})
 export class ProductoService {
-  private productos: Producto[] = [
-    { id: 1, nombre: 'Laptop', precio: 2500 },
-    { id: 2, nombre: 'Celular', precio: 1200 },
-  ];
-  constructor() {}
-  getProductos(): Producto[] {
-    return this.productos;
+  private xmlUrl = 'assets/productos.xml';
+  constructor(private http: HttpClient) {}
+  getProductos(): Observable<Producto[]> {
+    return this.http
+      .get(this.xmlUrl, { responseType: 'text' })
+      .pipe(map((xmlString) => this.parseXML(xmlString)));
+  }
+  private parseXML(xmlString: string): Producto[] {
+    const parser = new DOMParser();
+    const xml = parser.parseFromString(xmlString, 'application/xml');
+    const productos: Producto[] = [];
+    const nodes = xml.getElementsByTagName('producto');
+    for (let i = 0; i < nodes.length; i++) {
+      const node = nodes[i];
+      productos.push({
+        id: Number(node.getElementsByTagName('id')[0].textContent),
+        nombre: 'hola',
+        precio: 3000,
+      });
+    }
+    return productos;
   }
 }
