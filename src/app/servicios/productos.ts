1ff5ff0f69489dfba2c322bfe95fc1b475e6f340
JuanPichus <juanpichus123@gmail.com>
2025-10-14 20:18:13 -0600

    Punto de venta funcionando con comunicación con BD

---
+++ src/app/servicios/productos.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Productos {
+  private apiUrl = 'http://localhost:3000/api/catalogo';
+  constructor(private http: HttpClient) {}
+  getProductos(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/productos`);
+  }
+}
